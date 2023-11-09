5b7cda165e05370f41ebfba51712277edbdc0b36
joycegill <77594305+joycegill@users.noreply.github.com>
2023-11-09 13:48:44 -0600

    Update Hash.java

--- Hash.java
+++ Hash.java
@@ -1,16 +1,14 @@
-package src;
-
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.util.Arrays;
 import java.lang.*;
 
 /**
- * <Description>
- *
- * @author Joyce Gill
- * @author Maria Rodriguez
- */
+  * An implementation of Hash
+  *
+  * @author Joyce Gill
+  * @author Maria Rodriguez
+  */
 public class Hash {
   byte[] data;
 
@@ -21,36 +19,33 @@ public class Hash {
 
   /* Returns the hash contained in this object. */
   public byte[] getData(){
-    return this.data;
+    return Arrays.copyOf(this.data, this.data.length);
   } // getData()
 
   /* Returns true if this hash meets the criteria for validity, i.e., its first three indices contain zeroes. */
   public boolean isValid() {
-    if (data.length >= 3) {
+    if (data.length >= 3) { // legnth must be minimum 3
       if (data[0] == 0 && data[1] == 0 && data[2] == 0) {
         return true;
       } // if
     } // if
-    return false;
+    return false; // return false otherwise
   } // isValid()
 
   /* Returns the string representation of the hash as a string of hexadecimal digits, 2 digits per byte. */
   @Override
   public String toString() {
-    if (this.isValid()) {
-      String str = "";
-      for (byte b : data) {
-        int unsignedInt = Byte.toUnsignedInt(b);
-        str.concat(String.format("%2s", unsignedInt));
-      } // for
-      return str;
-    } // if
-    return null;
+    String str = "";
+    for (byte b : data) {
+      int unsignedInt = Byte.toUnsignedInt(b);
+      str.concat(String.format("%2s", unsignedInt));
+    } // for
+    return str;
   } // toString()
 
   /* Returns true if this hash is structurally equal to the argument. */
   public boolean equals(Object other) {
-    if (other.getClass() == Hash.class && this.isValid()) {
+    if (other.getClass() == Hash.class) {
       Hash o = (Hash) other;
       if (Arrays.equals(this.data, o.getData())) {
         return true;
