1dcad3215d0a8425f57b5ab78861aa554f4f33db
joycegill <77594305+joycegill@users.noreply.github.com>
2023-11-09 13:47:31 -0600

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,88 +1,89 @@
-package MP7.src;
-
 import java.nio.ByteBuffer;
-import java.security.DigestException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.NoSuchElementException;
 import java.util.Arrays;
 import java.lang.*;
 
 /**
- * <Description>
+ * An implementation for Block
  *
  * @author Joyce Gill
  * @author Maria Rodriguez
  */
 public class Block {
     
-/*---------------------------
- * Fields |
- * --------------------------
- */
+  /*---------------------------
+   * Fields |
+   * --------------------------
+   */
 
-    //the number of the block in the blockchain
-    int blockNum;
+  //the number of the block in the blockchain
+  int blockNum;
 
-    //the amt transferred between two parties
-    int amount;
+  //the amt transferred between two parties
+  int amount;
 
-    //the hash of the prev. block
-    Hash prevHash;
+  //the hash of the prev. block
+  Hash prevHash;
 
-    //the nonce
-    long nonce;
+  //the nonce
+  long nonce;
 
-    //the hash of this block
-    Hash hash;
+  //the hash of this block
+  Hash curHash;
 
 
-/*---------------------------
- * Constructor |
- * --------------------------
- */
+  /*---------------------------
+   * |      Constructor       |
+   * --------------------------
+   */
 
-    /*
-     * Block constructor that requires mining for a nonce
-     */
-    public Block(int num, int amount, Hash prevHash){
-        this.blockNum = num;
-        this.amount = amount;
-        this.prevHash = prevHash;
+  /*
+   * Block constructor that requires mining for a nonce
+   */
+  public Block(int num, int amount, Hash prevHash) {
+    this.blockNum = num;
+    this.amount = amount;
+    this.prevHash = prevHash;
 
-        //perform the mining operation for nonce and hash
-        try {
-            this.nonce = mine();
-        } catch (NoSuchAlgorithmException | DigestException e) {
-            e.printStackTrace();
-            System.err.println("No nonce was found");
-        }
+    //perform the mining operation for nonce and hash
+    try {
+      MessageDigest md = MessageDigest.getInstance("sha-256");
+      this.curHash = generateValidNonce(md);
+    } catch (Exception e) {
+      System.err.println("Cannot mine valid nonce! : " + e.getMessage());
     }
+  } // Block (int, amount, Hash)
 
-    /*
-     * Block constructor that is given a nonce
-     */
-    public Block(int num, int amount, Hash prevHash, long nonce){
-        this.blockNum = num;
-        this.amount = amount;
-        this.prevHash = prevHash;
-        this.nonce = nonce;
-
+  /*
+   * Block constructor that is given a nonce
+   */
+  public Block(int num, int amount, Hash prevHash, long nonce){
+    this.blockNum = num;
+    this.amount = amount;
+    this.prevHash = prevHash;
+    this.nonce = nonce;
+    try {
+      MessageDigest md = MessageDigest.getInstance("sha-256");
+      this.curHash = new Hash(generateGivenNonce(md, nonce));
+    } catch (Exception e) {
+      System.err.println("Cannot generate hash! : " + e.getMessage());
     }
+  } // Block (int, int, Hash, long)
 
 
-/*---------------------------
- * Methods |
- * --------------------------
- */
+  /*---------------------------
+   * |       Methods          |
+   * --------------------------
+   */
 
-    /*
-     * getNum()
-     * returns the block num
-     */
-    public int getNum(){
-        return this.blockNum;
-    }
+  /*
+   * getNum()
+   * returns the block num
+   */
+  public int getNum(){
+    return this.blockNum;
+  } // getNum()
 
     /*
      * getAmount()
@@ -90,80 +91,61 @@ public class Block {
      */
     public int getAmount(){
         return this.amount;
-    }
+    } // getAmount()
     
     /*
      * getNonce()
      * returns the nonce of this block
      */
     public long getNonce(){
-        return this.nonce;
-    }
+      return this.nonce;
+    } // getNonce()
 
     /*
      * getPrevHash()
      * returns the hash of the previous block in the blockchain
      */
     public Hash getPrevHash(){
-        return this.prevHash;
-    }
+      return this.prevHash;
+    } // getPrevHash ()
 
     /*
      * getHash()
      * returns the hash of this block
      */
     public Hash getHash() {
-        MessageDigest md;
-        try {
-            md = MessageDigest.getInstance("sha-256");
-            md.update(ByteBuffer.allocate(4).putInt(getNum()).array());
-            md.update(ByteBuffer.allocate(4).putInt(getAmount()).array());
-            if (getPrevHash() != null)
-                md.update(getPrevHash().toString().getBytes()); // Only when prev exists
-            md.update(ByteBuffer.allocate(8).putLong(nonce).array());
+      return this.curHash;
+    } // getHash ()
 
-            byte[] hashByte = md.digest();
-            Hash hashObj = new Hash(hashByte);
+    private Hash generateValidNonce(MessageDigest md) throws NoSuchAlgorithmException {
+      long i = 0;
+      Hash tmpHash = new Hash(generateGivenNonce(md, i));
+      while(!tmpHash.isValid()) {
+        tmpHash = new Hash(generateGivenNonce(md, ++i));
+      }//while
+      this.nonce = i;
+      return tmpHash;
+    } // mine ()
 
-            return hashObj;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }//getHash
-
-    long mine() throws NoSuchAlgorithmException, DigestException {
-        MessageDigest md = MessageDigest.getInstance("sha-256");
-
-        try {
-            md.update(ByteBuffer.allocate(4).putInt(getNum()).array());
-            md.update(ByteBuffer.allocate(4).putInt(getAmount()).array());
-            if (getPrevHash() != null)
-                md.update(getPrevHash().toString().getBytes()); // Only when prev exists
-            for (long i = 0; i < Long.MAX_VALUE - 1; i++) {
-                MessageDigest mdCopy = (MessageDigest) md.clone();
-                mdCopy.update(ByteBuffer.allocate(8).putLong(i).array());
-                byte[] hash = mdCopy.digest();
-
-                Hash tmpHash = new Hash(hash);
-                if (tmpHash.isValid()) {
-                    return i;
-                }
-            }
-            throw new NoSuchElementException();
-
-        } catch (CloneNotSupportedException cnse) {
-            throw new DigestException("couldn't make digest of partial content");
-        }
-    }//mine
+    private byte[] generateGivenNonce(MessageDigest md, long nonce){
+      md.reset();
+      md.update(ByteBuffer.allocate(4).putInt(this.getNum()).array());
+      md.update(ByteBuffer.allocate(4).putInt(this.getAmount()).array());
+      if (getPrevHash() != null)
+        md.update(getPrevHash().toString().getBytes()); // Only when prev exist
+      md.update(ByteBuffer.allocate(8).putLong(nonce).array());
+      byte[] temp = md.digest();
+      md.reset();
+      return temp;
+    } // generateGivenNonce(long)
 
     /*
      * toString()
      * returns a string representation of the block
      */
     public String toString(){
-        return "Block " + this.blockNum + " (Amount: " + this.amount + 
-        ", Nonce: " + this.nonce + ", prevHash: " + this.prevHash + ", hash: "
-        + this.hash + ")";
-    }
+      return "Block " + this.blockNum + " (Amount: " + this.amount + 
+      ", Nonce: " + this.nonce + ", prevHash: " + this.prevHash + ", hash: "
+      + this.curHash + ")";
+    } // toString()
 }
